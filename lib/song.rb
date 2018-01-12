938c94723daa8105cd770596418e3a37e73dc45d
Anna Yang <yanganna68@gmail.com>
2018-01-12 14:11:36 -0500

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+class Song
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    hash = {}
+    @@genres.each do |genre|
+      if hash[genre]
+        hash[genre] += 1
+      else
+        hash[genre] = 1
+      end
+    end
+    hash
+  end
+
+  def self.artist_count
+    hash = {}
+    @@artists.each do |artist|
+      if hash[artist]
+        hash[artist] += 1
+      else
+        hash[artist] = 1
+      end
+    end
+    hash
+  end
+
+
+end
